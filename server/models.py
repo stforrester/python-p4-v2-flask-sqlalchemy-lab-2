6e55984a6976b3f390c9a71070cd47636cecd168
Stuart Forrester <stforrester@gmail.com>
2025-01-23 04:56:56 -0600

    completes the lab and passes all tests

--- server/models.py
+++ server/models.py
@@ -11,22 +11,58 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+
+    serialize_rules = ('-reviews.customer',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    # Relationship mapping the customer to related reviews
+    reviews = db.relationship('Review', back_populates='customer', cascade='all, delete-orphan')
+
+    # Association proxy to get items for this customer through reviews
+    items = association_proxy('reviews', 'item', creator=lambda item_obj: Review(item=item_obj))
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
+
+    serialize_rules = ('-reviews.item',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # Relationship mapping the item to related reviews
+    reviews = db.relationship('Review', back_populates='item', cascade='all, delete-orphan')
+
+    # Association proxy to get customers for this item through reviews
+    customers = association_proxy('reviews', 'customer', creator=lambda customer_obj: Review(customer=customer_obj))
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    serialize_rules = ('-customer.reviews', '-item.reviews',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    # Foreign key to store the customer id
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    # Foreign key to store the item id
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    # Relationship mapping the review to related customer
+    customer = db.relationship('Customer', back_populates='reviews')
+    # Relationship mapping the review to related item
+    item = db.relationship('Item', back_populates='reviews')
+
+
